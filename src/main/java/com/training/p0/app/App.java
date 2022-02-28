e1039f444aeac07dce0b814dc7686b65adf1f797
Mark Kamradt <mark.kamradt@gmail.com>
2022-02-28 12:59:59 -0800

    fixing git structure

---
+++ src/main/java/com/training/p0/app/App.java
@@ -0,0 +1,18 @@
+package com.training.p0.app;
+
+import com.training.p0.consoleapp.ConsoleApp;
+
+//class App
+//
+//Banking application (console version) starts here.
+//
+//  Create ConsoleApp object.
+//  start
+
+public class App {
+	public static void main(String[] args) {
+		ConsoleApp consoleApp = new ConsoleApp();
+		consoleApp.start();
+	}
+
+}
